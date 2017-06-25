f5d1840f741f0a5f3d465fe2e3cbe187a3fb9894
Pakapon Kaewmark <sleep191@gmail.com>
2017-06-25 19:41:23 +0700

    Remove unused Package (Material)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'material-design-lite/material.min.css'
-import 'material-design-lite/material.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
