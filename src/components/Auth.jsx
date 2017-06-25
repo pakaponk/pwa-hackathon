5e3ed0381ec0f0cfcf6447d40b48a05328fb860c
Rachata Tosirisuk <rachatatosirisuk@gmail.com>
2017-06-24 21:45:33 -0700

    editing auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -5,18 +5,21 @@ const renderLogin = (props) => {
     if (props.user.isAuthenticated)
     {
         return (
-            <div>
-                <h3>Hello, {props.user.data.displayName}</h3>
-                <Link to="/rooms" className="mdl-button mdl-js-button mdl-button--raised">
-                    Let's Play a Quiz
-                </Link>
+            <div style={{'fontFamily':'Oswald'}}>
+                <h3>Hello !!!  {props.user.data.displayName}. Are you ready ?</h3>
+                <button style={{'border':'none','margin-top':'20px','padding':'10px'}}>
+                    <Link to="/rooms" className="button" style={{fontFamily:'Oswald','text-decoration':'none','font-size':'20'}}>
+                        Let's Play a Quiz
+                    </Link>
+                </button>
             </div>
         )
     }
+    
     else
     {
         return (
-            <button className="mdl-button mdl-js-button mdl-button--raised" onClick={(e) => props.handleLoginWithFacebook(e)}>
+            <button style={{fontFamily:'Oswald'}} class="text-center" onClick={(e) => props.handleLoginWithFacebook(e)}>
                 Login with Facebook
             </button>
         )
@@ -25,9 +28,12 @@ const renderLogin = (props) => {
 
 const Auth = (props) => {
     return (
-        <div className="mdl-typography--text-center" style={{paddingTop: '160px'}}>
-            <h1>Quiz app</h1>
-            { renderLogin(props) }
+        // <div className="mdl-typography--text-center" style={{paddingTop: '160px'}}>
+        <div className="text-center" style={{paddingTop: '200px'}}>
+            <h1> Quiz App</h1>
+            <div style={{paddingTop:'10px'}}>
+                { renderLogin(props) }
+            </div>
         </div>
     )
 }
